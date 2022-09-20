58506f034f9b8c7cc5b4897aced1f50af4c7c3c8
elisafvic <elisapersea@gmail.com>
2022-09-20 08:01:37 +0200

    Added functionality to check if there are cards remaining.

--- cardgame.js
+++ cardgame.js
@@ -25,10 +25,16 @@ let player2 = new Player("player2");
 
 // Create a new deck of cards and shuffle it.
 
-let mainDeck = new DeckOfCards(0, 20).shuffle();
+let mainDeck = new DeckOfCards(20, 20).shuffle();
+
+console.log("Cards are shuffled...")
+
+// Create deck for discards
+discardsDeck = [];
 
 // Assign cards to each player
 player1.hand = mainDeck.splice(0, 7);
+
 // If they already satisfy winning condition, return to deck, shuffle and assign again 
 while (checkIf5DifferentIngredients(player1.hand)) {
     mainDeck.push(player1.hand);
@@ -39,6 +45,7 @@ while (checkIf5DifferentIngredients(player1.hand)) {
 }
 
 player2.hand = mainDeck.splice(0, 7);
+
 while (checkIf5DifferentIngredients(player2.hand)) {
   mainDeck.push(player2.hand);
   mainDeck = mainDeck.flat().sort(function () {
@@ -47,5 +54,17 @@ while (checkIf5DifferentIngredients(player2.hand)) {
   player2.hand = mainDeck.splice(0, 7);
 }
 
-// Create deck for discards
-discardsDeck = [];
+console.log("Each player has 7 cards. We are ready to start playing!")
+
+// FIRST ROUND, PLAYER ONE
+
+// Let's check if there are cards in deck. If not, shuffle discards and reassign.
+
+if (mainDeck.length == 0) {
+  discards.flat().sort(function () {
+    return Math.random() - 0.5;
+  })
+  mainDeck = discards;
+  discards = [];
+  console.log("There are no cards remaining in the deck, so we have shuffled the discard pile.")
+}
